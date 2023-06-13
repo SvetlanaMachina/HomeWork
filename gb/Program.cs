50d8b7865a56b673fa1971ab6b3165ceb26a97c9
Svetlana <swetl.burlakova2014@yandex.ru>
2023-06-13 17:43:46 +0300

    Задача 2

---
+++ gb/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// напишите программу, которая нв вход принимает два числа и выдает какое большее, какое - меньшее
+
+Console.Write ("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine ($"Максимальное число {numberA}");
+}
+else
+{
+    Console.WriteLine ($"Максимальное число {numberB}");
+}
